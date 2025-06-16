a22a3b3ea4926b55d04dccce5e943a87b5bb2423
Yqzi <y.qazi12@gmail.com>
2025-06-16 00:28:37 -0600

    useState on useCount

--- client/src/app/components/user_count.tsx
+++ client/src/app/components/user_count.tsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import { FaUser, FaCrown } from "react-icons/fa";
 
 export default function UserCount() {
+    const [count, setCount] = useState<number>(1);
     return (
         <div className="flex flex-row mt-[60px]">
             <div className="bg-[#222225] w-[1px] h-full" />
             <div className="w-[260px] flex flex-col">
                 <div className="bg-[#222225] w-full h-[1px] mb-6" />
                 <div className="flex flex-col items-start text-gray-500 pl-4 space-y-1">
-                    <span className="text-sm">Online - 1</span>
+                    <span className="text-sm">Online - {count}</span>
                     <div className="flex flex-row items-center pl-2">
                         <div className="relative w-8 h-8">
                             <div className="w-8 h-8 rounded-full bg-gray-700 flex items-center justify-center text-white text-lg font-semibold">
